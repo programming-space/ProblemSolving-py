09a483281ea5e32d98d71a8b2779ed56a1933f78
yogeshraja08 <yogeshraja45@gmail.com>
2024-08-13 13:36:44 +0530

    yon changes

--- SnakeAndLadder.py
+++ SnakeAndLadder.py
@@ -30,6 +30,7 @@ def snake_and_ladder_game():
         if player2_position >= 100:
             print(f"Congratulations! {player2} wins!")
             break
+        
 snakes = {16: 6, 47: 26, 49: 11, 56: 53, 62: 19, 64: 60, 87: 24, 93: 73, 95: 75, 98: 78}
 ladders = {1: 38, 4: 14, 9: 31, 21: 42, 28: 84, 36: 44, 51: 67, 71: 91, 80: 100}
 snake_and_ladder_game()
